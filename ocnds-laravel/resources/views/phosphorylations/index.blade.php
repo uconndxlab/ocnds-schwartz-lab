e3c46a1bfeececa37de8af1d514030fbb56f3e4b
Joel Salisbury <joel.salisbury@gmail.com>
2024-08-19 13:29:56 -0400

    applied emelia's styling, added fade effects

--- ocnds-laravel/resources/views/phosphorylations/index.blade.php
+++ ocnds-laravel/resources/views/phosphorylations/index.blade.php
@@ -3,6 +3,7 @@
 
 @section('content')
 
+<div class="container">
     <div class="row">
         <div class="col-md-12">
             <h1>Phosphorylation</h1>
@@ -29,45 +30,45 @@
         </div>
     </div>
 
-    <div class="row" id="search_results">
+    <div class="row data" id="search_results">
         @foreach($phosphorylations as $phosphorylation)
             <div class="col-md-4">
                 <div class="card mb-4">
                     <div class="card-header">
-                        <h5 class="card-title">{{ $phosphorylation->accession }}</h5>
+                        <h5 class="card-title">Accession: {{ $phosphorylation->accession }}</h5>
                     </div>
                     <div class="card-body">
-                        <dl class="row">
-                            <dt class="col-sm-4">Accession</dt>
-                            <dd class="col-sm-8">{{ $phosphorylation->accession }}</dd>
-                            <dt class="col-sm-4">Position</dt>
-                            <dd class="col-sm-8">{{ $phosphorylation->position }}</dd>
-                            <dt class="col-sm-4">Gene Name</dt>
-                            <dd class="col-sm-8">{{ $phosphorylation->gene }}</dd>
-                            <dt class="col-sm-4">Gene Description</dt>
-                            <dd class="col-sm-8">{{ $phosphorylation->gene_description }}</dd>
-                            <dt class="col-sm-4">K198R S</dt>
-                            <dd class="col-sm-8">{{ $phosphorylation->k198r_s }}</dd>
-                            <dt class="col-sm-4">K198R M</dt>
-                            <dd class="col-sm-8">{{ $phosphorylation->k198r_m }}</dd>
-                            <dt class="col-sm-4">R47G</dt>
-                            <dd class="col-sm-8">{{ $phosphorylation->r47g }}</dd>
-                            <dt class="col-sm-4">D156E</dt>
-                            <dd class="col-sm-8">{{ $phosphorylation->d156e }}</dd>
-                            <dt class="col-sm-4">pval K198R S</dt>
-                            <dd class="col-sm-8">{{ $phosphorylation->pval_k198rs }}</dd>
-                            <dt class="col-sm-4">pval K198R M</dt>
-                            <dd class="col-sm-8">{{ $phosphorylation->pval_k198rm }}</dd>
-                            <dt class="col-sm-4">pval R47G</dt>
-                            <dd class="col-sm-8">{{ $phosphorylation->pval_r47g }}</dd>
-                            <dt class="col-sm-4">pval D156E</dt>
-                            <dd class="col-sm-8">{{ $phosphorylation->pval_d156e }}</dd>
-                            <dt class="col-sm-4">Modified Residue</dt>
-                            <dd class="col-sm-8">{{ $phosphorylation->modified_residue }}</dd>
-                            <dt class="col-sm-4">Modified Position</dt>
-                            <dd class="col-sm-8">{{ $phosphorylation->modified_position }}</dd>
-                            <dt class="col-sm-4">15mer</dt>
-                            <dd class="col-sm-8">{{ $phosphorylation->fifteen_mer }}</dd>
+                        <dl>
+                            <dt>Accession</dt>
+                            <dd>{{ $phosphorylation->accession }}</dd>
+                            <dt>Position</dt>
+                            <dd>{{ $phosphorylation->position }}</dd>
+                            <dt>Gene Name</dt>
+                            <dd>{{ $phosphorylation->gene }}</dd>
+                            <dt>Gene Description</dt>
+                            <dd>{{ $phosphorylation->gene_description }}</dd>
+                            <dt>K198R S</dt>
+                            <dd>{{ $phosphorylation->k198r_s }}</dd>
+                            <dt>K198R M</dt>
+                            <dd>{{ $phosphorylation->k198r_m }}</dd>
+                            <dt>R47G</dt>
+                            <dd>{{ $phosphorylation->r47g }}</dd>
+                            <dt>D156E</dt>
+                            <dd>{{ $phosphorylation->d156e }}</dd>
+                            <dt>pval K198R S</dt>
+                            <dd>{{ $phosphorylation->pval_k198rs }}</dd>
+                            <dt>pval K198R M</dt>
+                            <dd>{{ $phosphorylation->pval_k198rm }}</dd>
+                            <dt>pval R47G</dt>
+                            <dd>{{ $phosphorylation->pval_r47g }}</dd>
+                            <dt>pval D156E</dt>
+                            <dd>{{ $phosphorylation->pval_d156e }}</dd>
+                            <dt>Modified Residue</dt>
+                            <dd>{{ $phosphorylation->modified_residue }}</dd>
+                            <dt>Modified Position</dt>
+                            <dd>{{ $phosphorylation->modified_position }}</dd>
+                            <dt>15mer</dt>
+                            <dd>{{ $phosphorylation->fifteen_mer }}</dd>
                         </dl>
                     </div>
                 </div>
@@ -81,4 +82,5 @@
             {{ $phosphorylations->links('pagination::bootstrap-5') }}
         </div>
     </div>
+</div>
 @endsection

c66a657110d3340fd95e808d63483e72dc92d229
Joel Salisbury <joel.salisbury@gmail.com>
2024-08-19 10:39:00 -0400

    beginning the conversion to laravel

---
+++ ocnds-laravel/app/Http/Controllers/ProteinController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Protein;
+use Illuminate\Http\Request;
+
+class ProteinController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Protein $protein)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Protein $protein)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Protein $protein)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Protein $protein)
+    {
+        //
+    }
+}

2a921498017216497273b9b42cfaaf55cb3b51e9
Joel Salisbury <joel.salisbury@gmail.com>
2024-08-27 19:49:10 -0400

    added the react app

--- ocnds-laravel/routes/web.php
+++ ocnds-laravel/routes/web.php
@@ -9,3 +9,8 @@ Route::get('/', [ProteinController::class, 'index'])->name('proteins.index');
 
 // Route to show all phosphorylations
 Route::get('/phos', [PhosphorylationController::class, 'index'])->name('phosphorylations.index');
+
+Route::get('/ion-channel-predictor', function () {
+        return response()->file(public_path('scanx-okur-chung/build/index.html'));
+});
+
